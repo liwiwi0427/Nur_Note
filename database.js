58320ea18114229a6c2d9560dffb1d7a0c61edbb
liwiwi0427 <83771437+liwiwi0427@users.noreply.github.com>
2025-11-28 19:10:30 +0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,16 @@
+// 這是你的資料庫檔案
+window.nursingData = {
+    "Foley (導尿管)": {
+        "置入 (Insertion)": {
+            "一般置入": "Indwelling Foley catheter size {{尺寸}} Fr inserted smoothly. Urine output {{尿量}} ml, clear color. Balloon fixed with {{水量}} cc distilled water."
+        },
+        "護理 (Care)": {
+            "Foley Care": "Foley care done with aqueous beta-iodine. Patent flow. Fixed on {{大腿左/右}} thigh."
+        }
+    },
+    "NG Tube (鼻胃管)": {
+        "置入": {
+            "一般置入": "NG tube size {{尺寸}} Fr inserted up to {{公分}} cm. Confirmed placement by auscultation.",
+        }
+    }
+};
